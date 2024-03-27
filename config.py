1533f9c7df5690c365d1a3bb990be82743c22fce
dwtxaqgb1 <127172394+dwtxaqgb1@users.noreply.github.com>
2024-03-27 21:00:21 +0800

    更新 config.py

--- config.py
+++ config.py
@@ -1,19 +1,11 @@
 source_file = "demo.txt"
 final_file = "result.txt"
 favorite_list = [
-    "广东珠江",
-    "开平综合",
-    "开平生活",
+    
     "CCTV-1",
     "CCTV-5",
     "CCTV-5+",
     "CCTV-13",
-    "广东体育",
-    "广东卫视",
-    "大湾区卫视",
-    "浙江卫视",
-    "湖南卫视",
-    "翡翠台",
 ]
 favorite_page_num = 6
 default_page_num = 4
@@ -21,4 +13,4 @@ urls_limit = 15
 response_time_weight = 0.5
 resolution_weight = 0.5
 recent_days = 30
-ipv_type = "ipv4"
+ipv_type = "ipv6"
