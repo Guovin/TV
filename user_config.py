2c19d883109ee13a33ff497139ac8110ab4fd987
vbskycn <34542650+vbskycn@users.noreply.github.com>
2024-04-03 20:23:44 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,24 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "广东珠江",
+    "CCTV4",
+    "CCTV3",
+    "CCTV1",
+    "CCTV5",
+    "CCTV5+",
+    "CCTV13",
+    "广东体育",
+    "广东卫视",
+    "大湾区卫视",
+    "浙江卫视",
+    "湖南卫视",
+    "翡翠台",
+]
+favorite_page_num = 6
+default_page_num = 4
+urls_limit = 15
+response_time_weight = 0.6
+resolution_weight = 0.4
+recent_days = 30
+ipv_type = "ipv4"
