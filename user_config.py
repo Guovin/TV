cef6abbebb9e584b796d8ffb2fd5d4e334796e3f
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-04-04 21:59:49 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,29 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "CCTV1",
+    "CCTV2",
+    "CCTV3",
+    "CCTV4"，
+    "CCTV5"，
+    "CCTV6",
+    "CCTV7",
+    "CCTV8",
+    "CCTV9",
+    "CCTV10",
+    "CCTV11",
+    "CCTV12",
+    "CCTV13",
+    "CCTV14",
+    "CCTV15",
+    "CCTV16",
+    "CCTV17",
+    "海丰"
+]
+favorite_page_num = 8
+default_page_num = 8
+urls_limit = 15
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
