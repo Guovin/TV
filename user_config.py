a557743a42efbbfb1208a9092257017ff83d1876
jied8886 <84348658@qq.com>
2024-04-04 09:54:46 +0800

    Create user_config.py
    Signed-off-by: jied8886 <84348658@qq.com>

---
+++ user_config.py
@@ -0,0 +1,22 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "CCTV1",
+    "CCTV5",
+    "CCTV5+",
+    "CCTV13",
+      "珠江",
+    "开平综合",
+    "开平生活",
+    "广东体育",
+    "广东卫视",
+    "大湾区卫视",
+    "浙江卫视",
+    "湖南卫视",
+    "翡翠台",
+]
+favorite_page_num = 5
+default_page_num = 3
+urls_limit = 8
+response_time_weight = 0.5
+resolution_weight = 0.5
