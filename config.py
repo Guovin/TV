978c8a1627467e8d253ca4d0f1508b463e5a0e78
dwtxaqgb1 <127172394+dwtxaqgb1@users.noreply.github.com>
2024-04-08 10:49:26 +0800

    更新 config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ favorite_list = [
     "CCTV-5+",
     "CCTV-13",
 ]
-favorite_page_num = 6
-default_page_num = 4
-urls_limit = 15
+favorite_page_num = 2
+default_page_num = 2
+urls_limit = 2
 response_time_weight = 0.5
 resolution_weight = 0.5
 recent_days = 30
