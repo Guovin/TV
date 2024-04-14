9346712ec3fbd3568c100926b0d6256f7de14cce
jied8886 <84348658@qq.com>
2024-04-14 09:30:44 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,26 @@
+source_file = "demo.txt"
+final_file = "result.txt"
+favorite_list = [
+    "CCTV-1",
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-13",
+    "广东珠江",
+    "开平综合",
+    "开平生活",
+    "广东体育",
+    "广东卫视",
+    "大湾区卫视",
+    "浙江卫视",
+    "湖南卫视",
+    "翡翠台",
+]
+favorite_page_num = 3
+default_page_num = 2
+urls_limit = 10
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
