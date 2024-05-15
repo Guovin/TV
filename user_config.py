53d56325eb54daaec1278b45549c69b5ce81ca9b
jy404 <liujianyuangood@gmail.com>
2024-05-15 17:56:28 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,35 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "广东珠江",
+    "CCTV-1",
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-13",
+    "广东体育",
+    "广东卫视",
+    "大湾区卫视",
+    "浙江卫视",
+    "湖南卫视",
+    "翡翠台",
+]
+open_online_search = True
+favorite_page_num = 5
+default_page_num = 3
+urls_limit = 10
+open_sort = True
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
+open_subscribe = True
+subscribe_urls = [
+    "https://m3u.ibert.me/txt/fmml_dv6.txt",
+    "https://m3u.ibert.me/txt/o_cn.txt",
+    "https://m3u.ibert.me/txt/j_iptv.txt",
+]
+open_multicast = True
+region_list = ["广东"]
+strict_match = False
