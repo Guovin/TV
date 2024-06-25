5758094e7c4dc2de1883fbd5d3212c8a83059250
ajun59420 <ajun59420@gmail.com>
2024-06-25 17:23:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,9 @@ domain_blacklist = ["epg.pw"]
 url_keywords_blacklist = []
 open_subscribe = True
 subscribe_urls = [
-    "https://raw.gitmirror.com/jdliu243910/zb/main/v4mu",
+    "https://m3u.ibert.me/txt/fmml_dv6.txt",
+    "https://m3u.ibert.me/txt/o_cn.txt",
+    "https://m3u.ibert.me/txt/j_iptv.txt",
 ]
 open_multicast = True
 region_list = ["all"]
