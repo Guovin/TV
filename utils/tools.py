db57d7634bd66185ea89000b3ee796955a987257
guorong.zheng <360996299@qq.com>
2024-11-05 11:15:30 +0800

    fix:not with origin(#505)

--- utils/tools.py
+++ utils/tools.py
@@ -140,9 +140,9 @@ def get_total_urls_from_info_list(infoList, ipv6=False):
             continue
 
         if origin == "important":
-            pure_url, _, info = url.partition("$")
-            new_info = info.partition("!")[2]
-            total_urls.append(f"{pure_url}${new_info}" if new_info else pure_url)
+            im_url, _, im_info = url.partition("$")
+            im_info_value = im_info.partition("!")[2]
+            total_urls.append(f"{im_url}${im_info_value}" if im_info_value else im_url)
             continue
 
         if origin == "subscribe" and "/rtp/" in url:
@@ -156,9 +156,13 @@ def get_total_urls_from_info_list(infoList, ipv6=False):
             if resolution_value < config.min_resolution_value:
                 continue
 
+        pure_url, _, info = url.partition("$")
+        if not info:
+            url = add_url_info(pure_url, origin)
+
         url_is_ipv6 = is_ipv6(url)
         if url_is_ipv6:
-            url += "|IPv6"
+            url = add_url_info(url, "IPv6")
 
         if url_is_ipv6:
             categorized_urls[origin]["ipv6"].append(url)
