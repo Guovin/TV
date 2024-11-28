e1d97bd4d995f673112c3894f52e68363f5edb0b
guorong.zheng <360996299@qq.com>
2024-11-28 15:26:17 +0800

    chore:app result

--- utils/tools.py
+++ utils/tools.py
@@ -406,14 +406,13 @@ def get_result_file_content(show_content=False, file_type=None):
         else user_final_file
     )
     if os.path.exists(result_file):
-        if file_type == "txt" or not config.open_m3u_result:
-            with open(result_file, "r", encoding="utf-8") as file:
-                content = file.read()
-        elif config.open_m3u_result:
-            if not file_type:
+        if config.open_m3u_result:
+            if file_type == "m3u" or not file_type:
                 result_file = os.path.splitext(user_final_file)[0] + ".m3u"
-            if show_content == False:
+            if file_type != "txt" and show_content == False:
                 return send_file(result_file, as_attachment=True)
+        with open(result_file, "r", encoding="utf-8") as file:
+            content = file.read()
     else:
         content = constants.waiting_tip
     return render_template_string(
