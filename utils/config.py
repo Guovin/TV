c6c5b198e17f14fcbad721fe58c633183156adce
guorong.zheng <360996299@qq.com>
2024-12-17 10:01:19 +0800

    chore:tkinter config

--- utils/config.py
+++ utils/config.py
@@ -339,43 +339,18 @@ class ConfigManager:
         """
         Copy config files to current directory
         """
-        user_source_file = resource_path(
-            self.config.get("Settings", "source_file", fallback="config/demo.txt")
-        )
-        user_config_path = resource_path("config/user_config.ini")
-        default_config_path = resource_path("config/config.ini")
-        user_config_file = (
-            user_config_path
-            if os.path.exists(user_config_path)
-            else default_config_path
-        )
         dest_folder = os.path.join(os.getcwd(), "config")
-        files_to_copy = [user_source_file, user_config_file]
         try:
-            if os.path.exists(dest_folder):
-                if not os.path.isdir(dest_folder):
-                    os.remove(dest_folder)
+            src_dir = resource_path("config")
+            if os.path.exists(src_dir):
+                if not os.path.exists(dest_folder):
                     os.makedirs(dest_folder, exist_ok=True)
-            else:
-                os.makedirs(dest_folder, exist_ok=True)
-            for src_file in files_to_copy:
-                dest_path = os.path.join(dest_folder, os.path.basename(src_file))
-                if os.path.abspath(src_file) == os.path.abspath(
-                        dest_path
-                ) or os.path.exists(dest_path):
-                    continue
-                shutil.copy(src_file, dest_folder)
-            src_rtp_dir = resource_path("config/rtp")
-            dest_rtp_dir = os.path.join(dest_folder, "rtp")
-            if os.path.exists(src_rtp_dir):
-                if not os.path.exists(dest_rtp_dir):
-                    os.makedirs(dest_rtp_dir, exist_ok=True)
 
-                for root, _, files in os.walk(src_rtp_dir):
+                for root, _, files in os.walk(src_dir):
                     for file in files:
                         src_file_path = os.path.join(root, file)
-                        relative_path = os.path.relpath(src_file_path, src_rtp_dir)
-                        dest_file_path = os.path.join(dest_rtp_dir, relative_path)
+                        relative_path = os.path.relpath(src_file_path, src_dir)
+                        dest_file_path = os.path.join(dest_folder, relative_path)
 
                         dest_file_dir = os.path.dirname(dest_file_path)
                         if not os.path.exists(dest_file_dir):
